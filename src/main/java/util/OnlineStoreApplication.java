4e4038aa0538d38eb306d3c17d8048ce247b96c3
Fatemeh-Heidari2024 <erfana.info@gmail.com>
2024-06-12 16:09:06 +0330

    create OnlineStoreApplication class

---
+++ src/main/java/util/OnlineStoreApplication.java
@@ -0,0 +1,10 @@
+package util;
+
+import entity.User;
+
+public class OnlineStoreApplication {
+    public static void main(String[] args) {
+        ApplicationContext.getInstance().getMenu().show();
+        User user=new User();
+    }
+}
